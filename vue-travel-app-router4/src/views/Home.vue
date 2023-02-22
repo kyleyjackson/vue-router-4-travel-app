7a932da3e275a644f9b3a1ac173382d25baeb97f
kyleyjackson <kjackson@bayviewglen.ca>
2023-02-22 12:24:04 -0500

    create dist

---
+++ vue-travel-app-router4/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="home">
+        <h1>All Destinations</h1>
+        <div class="destinations"> 
+            <router-link
+            v-for="destination in destinations" 
+            :key="destination.id"
+            :to="destination.slug"
+            > <!-- v-for loops through each {destination} in {destinations} (kinda like forEach) -->
+            <!-- :key attribute tells Vue how {destination} relates to the HTML elements being rendered -->
+                <h2>{{destination.name}}</h2>
+                <img :src="`/images/${destination.image}`" alt="destination.name">
+            </router-link>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import sourceData from '../data.json'
+export default {
+    data() {
+        return {
+            destinations: sourceData.destinations
+        }
+    }
+}
+</script>
